65d126d8c734a70b45c7a9ba2975f4c51adc3b1c
Billy <billyjcatchpole@gmail.com>
2020-07-13 06:51:14 +0100

    Added alphabetical sorting on the returned notes and processes based on their title Added 'both' array which is sorted notes and processes combined

--- controllers/7GetAll.js
+++ controllers/7GetAll.js
@@ -3,9 +3,7 @@ const Process = require("../models//Process")//import the process schema to inte
 
 exports.get_all = async (req, res, next) => {
 
-    console.log(req.body)
-
-    if(!req.body.user_id) return res.status(400).json({ message: "Bad request" })//if the title is missing, send a 424 and inform the user
+    if (!req.body.user_id) return res.status(400).json({ message: "Bad request" })//if the title is missing, send a 424 and inform the user
 
     const user_id = req.body.user_id;//extract the user id from the request body
 
@@ -13,20 +11,26 @@ exports.get_all = async (req, res, next) => {
 
         const notes_fetched = await Note.find({
 
-            $or:[//either of the following criteria will return a match
+            $or: [//either of the following criteria will return a match
                 { created_by: user_id },//created by the user ?
-                {access_rights: { $elemMatch: { _id: user_id } }}//User has access rights to the note?
-            ]})//fetch all notes which were created by the given user
+                { access_rights: { $elemMatch: { _id: user_id } } }//User has access rights to the note?
+            ]
+        })//fetch all notes which were created by the given user
 
         const processes_fetched = await Process.find({
 
-            $or:[//either of the following criteria will return a match
+            $or: [//either of the following criteria will return a match
                 { created_by: user_id },//created by the user ?
-                {access_rights: { $elemMatch: { _id: user_id } }}//User has access rights to the note?
-            ]})//fetch all notes which were created by the given user
+                { access_rights: { $elemMatch: { _id: user_id } } }//User has access rights to the note?
+            ]
+        })//fetch all notes which were created by the given user
 
-        //once the notes have been fetched (even if 0 was found)
-        notes_fetched && processes_fetched && res.status(200).json({ message: "Notes and processes retrieved", notes:notes_fetched, processes:processes_fetched})//return a 200 with all found notes attached
+        const sorted_notes = notes_fetched.sort((a,b) => (a.title > b.title) ? 1 : -1)//sort the notes in alphabetical based on title
+        const sorted_processes = processes_fetched.sort((a,b) => (a.title > b.title) ? 1 : -1)//sort the processes in alphabetical based on title
+        const sorted_both = sorted_notes.concat(sorted_processes).sort((a,b) => (a.title > b.title) ? 1 : -1)//sort everything in alphabetical based on title
+
+            //once the notes have been fetched (even if 0 was found)
+            notes_fetched && processes_fetched && res.status(200).json({ message: "Notes and processes retrieved", notes: sorted_notes, processes: sorted_processes, both:sorted_both })//return a 200 with all found notes attached
 
     }
 
