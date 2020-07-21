35a5ae13ab95ef7915d8da82c38844c31cee840c
Billy <billyjcatchpole@gmail.com>
2020-07-21 12:56:51 +0100

    Added the frontend url to the reset password email

--- util/send_email.js
+++ util/send_email.js
@@ -24,9 +24,9 @@ exports.password_reset = (email, token, user_id) => {
            "Subject": "Recall password reset",//subject
            "HTMLPart": `
            <h3>  You requested a password reset email </h3>
-           <h5> If this was not you, please ignore this email </h5>
+           <h5> If this was not you, please ignore this email and secure your account and devices </h5>
  
-           <p> Click this <a href="frontendurl/${token}/?userId=${user_id}">link</a> to reset your password</p>
+           <p> Click this <a href="https://get-recall.web.app/change_password/${token}/?userId=${user_id}">link</a> to reset your password</p>
            `,
          }
        ]
